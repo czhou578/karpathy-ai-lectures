79eb17a680c333c6f8d28cc1bcac3739d32d50ff
czhou578 <colizu2020@gmail.com>
2024-06-09 22:47:39 -0700

    commit

---
+++ tokenizer/tokenize.py
@@ -0,0 +1,11 @@
+
+class BasicTokenizer():
+    def train(self, text, vocab_size, verbose=False):
+        num_merges = vocab_size - 200
+
+
+
+    def encode(self, text):
+        tokens = b''.join(vocab[idx] for idx in text)
+
+    def decode(self, text):
